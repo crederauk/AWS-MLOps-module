31dcc6cc51818775590281254820f893d94c9d99
BenWatson2000 <ben.thompson-watson@credera.co.uk>
2024-10-31 16:22:46 +0000

    chore: added 'shap' to dependencies

--- modules/glue/glue_jobs/setup.py
+++ modules/glue/glue_jobs/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name="mlops_dependencies_module",
     version="0.1",
-    install_requires=['pylint','pytest','flake8','boto3', 'pandas', 'requests', 'sagemaker', 'python-dotenv', 'pycaret', 'tensorflow', 'gunicorn', 'joblib']
+    install_requires=['pylint','pytest','flake8','boto3', 'pandas', 'requests', 'sagemaker', 'python-dotenv', 'pycaret', 'tensorflow', 'gunicorn', 'joblib', 'shap']
 )
 
 # run python setup.py bdist_wheel to build .whl file

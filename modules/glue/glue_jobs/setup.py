d7ebdf41abf170e77f281e3bb3e4de1d6a606ba8
BenWatson2000 <ben.thompson-watson@credera.co.uk>
2024-11-01 14:10:33 +0000

    chore: update fsspec to 2023.5.0

--- modules/glue/glue_jobs/setup.py
+++ modules/glue/glue_jobs/setup.py
@@ -3,5 +3,5 @@ from setuptools import setup
 setup(
     name="mlops_dependencies_module",
     version="0.1",
-    install_requires=['pylint','pytest','flake8','boto3', 'pandas==2.1.4', 'requests', 'sagemaker', 'python-dotenv', 'pycaret', 'tensorflow', 'gunicorn', 'joblib', 'shap']
+    install_requires=['pylint','pytest','flake8','boto3', 'pandas', 'requests', 'sagemaker', 'python-dotenv', 'pycaret', 'tensorflow', 'gunicorn', 'joblib', 'shap', 'fsspec==2023.5.0']
 )

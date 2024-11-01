eaeb59ee72100ab5e0b644753b86966f0b457706
BenWatson2000 <ben.thompson-watson@credera.co.uk>
2024-11-01 13:04:33 +0000

    chore: added pandas to .whl file

--- modules/glue/glue_jobs/setup.py
+++ modules/glue/glue_jobs/setup.py
@@ -3,5 +3,5 @@ from setuptools import setup
 setup(
     name="mlops_dependencies_module",
     version="0.1",
-    install_requires=['pylint','pytest','flake8','boto3', 'pandas', 'requests', 'sagemaker', 'python-dotenv', 'pycaret', 'tensorflow', 'gunicorn', 'joblib', 'shap']
+    install_requires=['pylint','pytest','flake8','boto3', 'pandas', 'requests', 'sagemaker', 'python-dotenv', 'pycaret', 'tensorflow', 'gunicorn', 'joblib', 'shap', 'pandas']
 )

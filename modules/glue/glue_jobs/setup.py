2224fbee0e598098ae9f44bb8fd3b5832f1c4163
BenWatson2000 <ben.thompson-watson@credera.co.uk>
2024-11-01 13:39:40 +0000

    chore: specify pandas version to be 2022.5.0

--- modules/glue/glue_jobs/setup.py
+++ modules/glue/glue_jobs/setup.py
@@ -3,5 +3,5 @@ from setuptools import setup
 setup(
     name="mlops_dependencies_module",
     version="0.1",
-    install_requires=['pylint','pytest','flake8','boto3', 'pandas', 'requests', 'sagemaker', 'python-dotenv', 'pycaret', 'tensorflow', 'gunicorn', 'joblib', 'shap', 'pandas']
+    install_requires=['pylint','pytest','flake8','boto3', 'pandas==2022.5.0', 'requests', 'sagemaker', 'python-dotenv', 'pycaret', 'tensorflow', 'gunicorn', 'joblib', 'shap']
 )

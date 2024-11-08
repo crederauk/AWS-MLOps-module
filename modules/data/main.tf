4b3eed7b0312daedf2103ccea41df472dd0e095c
HarryBurnhamCredera <harry.burnham@credera.co.uk>
2024-11-08 13:34:18 +0000

    removing for each

--- modules/data/main.tf
+++ modules/data/main.tf
@@ -1,14 +1,11 @@
 locals {
   file_path = "${path.module}/data_files"
-  files_to_upload = concat(
-    tolist(fileset(local.file_path, "*.csv")),
-  )
+  file_to_upload = fileset(local.file_path, "*.csv"),
 
   bucket_name = var.data_bucket_id
 }
 
 resource "aws_s3_bucket_object" "data_files" {
-  for_each = toset(local.files_to_upload)
   bucket   = local.bucket_name
   key      = "data_files/${each.value}"
   source   = "${local.file_path}/${each.value}"
--- modules/data/outputs.tf
+++ modules/data/outputs.tf
@@ -1,3 +1,3 @@
 output "file_name" {
-  value = aws_s3_bucket_object[0].key
+  value = aws_s3_bucket_object.key
 }

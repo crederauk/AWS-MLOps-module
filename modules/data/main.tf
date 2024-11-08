a17e2da84ee278358206514771962aaf26e341c2
HarryBurnhamCredera <harry.burnham@credera.co.uk>
2024-11-08 13:43:16 +0000

    Changing from for each

--- modules/data/main.tf
+++ modules/data/main.tf
@@ -1,14 +1,14 @@
 locals {
   file_path = "${path.module}/data_files"
   file_to_upload = fileset(local.file_path, "*.csv"),
-
+  
   bucket_name = var.data_bucket_id
 }
 
 resource "aws_s3_bucket_object" "data_files" {
   bucket   = local.bucket_name
-  key      = "data_files/${each.value}"
-  source   = "${local.file_path}/${each.value}"
-  etag     = filemd5("${path.module}/data_files/${each.value}")
+  key      = "data_files/${local.file_to_upload}"
+  source   = "${local.file_path}/${local.file_to_upload}"
+  etag     = filemd5("${path.module}/data_files/${local.file_to_upload}")
   tags     = var.tags
 }

3e1f389867ccd9ca19e74dd49a80f4ffa42b53bb
HarryBurnhamCredera <harry.burnham@credera.co.uk>
2024-11-08 12:17:49 +0000

    Fixing mistake

--- data/main.tf
+++ data/main.tf
@@ -9,7 +9,7 @@ locals {
 
 resource "aws_s3_bucket_object" "data_files" {
   for_each = toset(local.files_to_upload)
-  bucket   =  local.bucket_name
+  bucket   = local.bucket_name
   key      = "data_files/${each.value}"
   source   = "${local.file_path}/${each.value}"
   etag     = filemd5("${path.module}/data_files/${each.value}")

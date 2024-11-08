f04697a6543fd678f6ce6f1465fe9c36df38a526
HarryBurnhamCredera <harry.burnham@credera.co.uk>
2024-11-08 14:01:34 +0000

    update

--- modules/data/main.tf
+++ modules/data/main.tf
@@ -1,14 +1,14 @@
 locals {
-  file_path = "${path.module}/data_files"
+  file_path = "${path.module}/data_file"
   file_to_upload = fileset(local.file_path, "*.csv")
   
   bucket_name = var.data_bucket_id
 }
 
-resource "aws_s3_bucket_object" "data_files" {
+resource "aws_s3_bucket_object" "data_file" {
   bucket   = local.bucket_name
   key      = "data_files/${local.file_to_upload}"
   source   = "${local.file_path}/${local.file_to_upload}"
-  etag     = filemd5("${path.module}/data_files/${local.file_to_upload}")
+  etag     = filemd5("${path.module}/data_file/${local.file_to_upload}")
   tags     = var.tags
 }
--- modules/data/outputs.tf
+++ modules/data/outputs.tf
@@ -1,3 +1,3 @@
 output "file_name" {
-  value = aws_s3_bucket_object.key
+  value = aws_s3_bucket_object.data_file.key
 }
